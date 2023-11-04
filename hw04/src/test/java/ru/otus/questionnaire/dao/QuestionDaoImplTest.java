c4650ba76da390987e0864b8f13e379099fa59e6
artem.kobyakov <a.kobiakov@bftcom.com>
2023-11-04 17:49:21 +0300

    add @DisplayName to QuestionDaoImplTest

--- hw04/src/test/java/ru/otus/questionnaire/dao/QuestionDaoImplTest.java
+++ hw04/src/test/java/ru/otus/questionnaire/dao/QuestionDaoImplTest.java
@@ -1,5 +1,6 @@
 package ru.otus.questionnaire.dao;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,6 +13,7 @@ import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@DisplayName("QuestionDao ")
 @SpringBootTest(classes = QuestionDaoImpl.class)
 class QuestionDaoImplTest {
 
@@ -21,6 +23,7 @@ class QuestionDaoImplTest {
     @MockBean
     private FileNameProvider fileNameProvider;
 
+    @DisplayName(" should find all questions")
     @Test
     void findAll() {
         Mockito.when(fileNameProvider.getTestFileName()).thenReturn("questions_ru.csv");
